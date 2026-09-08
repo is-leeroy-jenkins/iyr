8d1824777e2951dcd4eccf187affa5c81fe29025
is-leeroy-jenkins <terry.eppler@gmail.com>
2026-09-08 14:36:38 -0400

    updates

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ import streamlit.components.v1 as components
 
 # ------------ CONSTANT
 BLUE_DIVIDER = "<div style='height:1.5px;align:left;background:#0078FC;margin:20px 0px 30px 0px;'></div>"
-APP_TITLE = 'ayin'
+APP_TITLE = 'iyr'
 APP_SUBTITLE = 'Geospatial Toolkit'
 DB_PATH = 'stores/sqlite/data.db'
 DEFAULT_DATA = r'Reports'
